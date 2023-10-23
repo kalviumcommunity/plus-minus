81e349b097cfbfddec67db94d7aa0d909bb547dd
imadityaanand <imadityaanand24@gmail.com>
2023-10-23 18:17:59 +0530

    Grid numbers fill randomly implement

--- 24 Game/Assets/Scripts/DatabaseManager.cs
+++ 24 Game/Assets/Scripts/DatabaseManager.cs
@@ -1,51 +1,51 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using MySql.Data.MySqlClient;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using MySql.Data.MySqlClient;
 
-public class DatabaseManager : MonoBehaviour
-{
-    private MySqlConnection connection;
-    private string serverAddress = "your_server_address";
-    private string databaseName = "your_database_name";
-    private string username = "your_username";
-    private string password = "your_password";
+// public class DatabaseManager : MonoBehaviour
+// {
+//     private MySqlConnection connection;
+//     private string serverAddress = "http://localhost:3306";
+//     private string databaseName = "plusminus";
+//     private string username = "root";
+//     private string password = "$Hutkone321";
 
-    private void Start()
-    {
-        string connectionString = $"Server={serverAddress};Database={databaseName};User={username};Password={password};";
+//     private void Start()
+//     {
+//         string connectionString = $"Server={serverAddress};Database={databaseName};User={username};Password={password};";
 
-        try
-        {
-            connection = new MySqlConnection(connectionString);
-            connection.Open();
+//         try
+//         {
+//             connection = new MySqlConnection(connectionString);
+//             connection.Open();
 
-            // Create a new table named 'YourTableName'
-            string createTableQuery = "sample_sql_query";
+//             // Create a new table named 'YourTableName'
+//             string createTableQuery = "sample_sql_query";
 
-            MySqlCommand cmd = new MySqlCommand(createTableQuery, connection);
-            cmd.ExecuteNonQuery();
+//             MySqlCommand cmd = new MySqlCommand(createTableQuery, connection);
+//             cmd.ExecuteNonQuery();
 
-            Debug.Log("Table created successfully.");
-        }
-        catch (Exception e)
-        {
-            Debug.LogError("Error: " + e.Message);
-        }
-        finally
-        {
-            if (connection != null && connection.State == ConnectionState.Open)
-            {
-                connection.Close();
-            }
-        }
-    }
+//             Debug.Log("Table created successfully.");
+//         }
+//         catch (Exception e)
+//         {
+//             Debug.LogError("Error: " + e.Message);
+//         }
+//         finally
+//         {
+//             if (connection != null && connection.State == ConnectionState.Open)
+//             {
+//                 connection.Close();
+//             }
+//         }
+//     }
 
-    private void OnDestroy()
-    {
-        if (connection != null && connection.State == ConnectionState.Open)
-        {
-            connection.Close();
-        }
-    }
-}
+//     private void OnDestroy()
+//     {
+//         if (connection != null && connection.State == ConnectionState.Open)
+//         {
+//             connection.Close();
+//         }
+//     }
+// }
---
+++ 24 Game/Assets/Scripts/GridManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class GridManager : MonoBehaviour
+{
+    public GameObject[] textContainers; // Assign your parent GameObjects in the Inspector.
+    private TextMeshProUGUI[] textMeshPros; // Array to store TextMeshPro components.
+
+    private void Start()
+    {
+        InitializeTextMeshPros();
+        SetRandomText();
+    }
+
+    // Initialize the static array of TextMeshPro components.
+    private void InitializeTextMeshPros()
+    {
+        textMeshPros = new TextMeshProUGUI[textContainers.Length];
+
+        for (int i = 0; i < textContainers.Length; i++)
+        {
+            textMeshPros[i] = textContainers[i].GetComponentInChildren<TextMeshProUGUI>();
+        }
+    }
+
+    public void SetRandomText()
+    {
+        foreach (TextMeshProUGUI textMeshPro in textMeshPros)
+        {
+            if (textMeshPro != null)
+            {
+                int randomValue = Random.Range(0, 2) * 2 + 2;
+                textMeshPro.text = randomValue.ToString();
+            }
+        }
+        Debug.Log("Text set.");
+    }
+}
